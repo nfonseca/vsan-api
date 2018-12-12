7b4fd52b6514cb286f16846d6d7a04900d075105
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2018-12-12 15:59:35 +0000

    initial commit

---
+++ vsan-api.py
@@ -0,0 +1,5 @@
+# First commit to start the project
+
+# todo - add a connect function to VC using argparse
+# todo - main goal of the tool is to gather config information from the cluster
+# todo - Goal 1: Get Cluster Configuration
